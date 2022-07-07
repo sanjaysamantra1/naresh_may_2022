a3a710177050978741a2f5f6bff5b281db2fbde4
Sanjay Samantra <sanjaysamantra1@gmail.com>
2022-07-07 08:07:55 +0530

    second change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React From NareshIT
+          Learn React
         </a>
       </header>
     </div>
